10f74b733e38042913ee176d2614302551db9c9c
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-04 03:09:19 +0600

    Minimun 3 reviews in the home page

--- src/Components/CustomerReview/CustomerReview.js
+++ src/Components/CustomerReview/CustomerReview.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useReviews from '../../Hooks/useReviews';
 
 const CustomerReview = () => {
+
+    const [reviews] = useReviews();
+    const [review1, review2, review3] = reviews;
+
     return (
         <div className='my-10'>
             <h1 className='text-4xl text-center font-semibold'>Our Product Reviews</h1>
+            <div className='grid lg:grid-cols-3 gap-20 my-16'>
+
+                <div className='p-5 h-96 rounded-lg border-2'>
+                    <img className='mx-auto rounded-full my-4' src={review1?.img} alt="" />
+                    <h2 className='text-xl font-medium my-7 text-center'>{review1?.name}</h2>
+                    <p title={review1?.review}>{review1?.review.length > 200 ? review1?.review.slice(0, 200) + "....." : review1?.review}</p>
+                </div>
+
+                <div className='p-5 h-96 rounded-lg border-2'>
+                    <img className='mx-auto rounded-full my-4' src={review2?.img} alt="" />
+                    <h2 className='text-xl font-medium my-7 text-center'>{review2?.name}</h2>
+                    <p title={review2?.review}>{review2?.review.length > 200 ? review2?.review.slice(0, 200) + "....." : review2?.review}</p>
+                </div>
+
+                <div className='p-5 h-96 rounded-lg border-2'>
+                    <img className='mx-auto rounded-full my-4' src={review3?.img} alt="" />
+                    <h2 className='text-xl font-medium my-7 text-center'>{review3?.name}</h2>
+                    <p title={review3?.review}>{review3?.review.length > 200 ? review3?.review.slice(0, 200) + "....." : review3?.review}</p>
+                </div>
+            </div>
         </div>
     );
 };
