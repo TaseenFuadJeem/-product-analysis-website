0ac7040b5b2b008a9c60212930bc65c81b240a71
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-03 23:38:36 +0600

    Home Component half completed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
-    <div>
+    <div className='px-32'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
+import SonyLogo from '../../Assets/sony-logo.png';
 
 const Navbar = () => {
     return (
-        <nav className='text-center my-10'>
-            <NavLink className='mx-5 font-bold' to="/home">HOME</NavLink>
-            <NavLink className='mx-5 font-bold' to="/reviews">REVIEWS</NavLink>
-            <NavLink className='mx-5 font-bold' to="/dashboard">DASHBOARD</NavLink>
-            <NavLink className='mx-5 font-bold' to="/blogs">BLOGS</NavLink>
-            <NavLink className='mx-5 font-bold' to="/about">ABOUT</NavLink>
-        </nav>
+        <div className='flex items-center justify-between my-10'>
+            <div>
+                <img className='w-40' src={SonyLogo} alt="" />
+            </div>
+            <nav className=''>
+                <NavLink className='mx-5 font-bold' to="/home">HOME</NavLink>
+                <NavLink className='mx-5 font-bold' to="/reviews">REVIEWS</NavLink>
+                <NavLink className='mx-5 font-bold' to="/dashboard">DASHBOARD</NavLink>
+                <NavLink className='mx-5 font-bold' to="/blogs">BLOGS</NavLink>
+                <NavLink className='mx-5 font-bold' to="/about">ABOUT</NavLink>
+            </nav>
+        </div>
     );
 };
 
