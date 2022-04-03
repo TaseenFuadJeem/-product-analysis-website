013c27aa646ef9f8ecc70b1af12ce2bd136e7660
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-04 03:34:11 +0600

    Review star icon added

--- src/Components/CustomerReview/CustomerReview.js
+++ src/Components/CustomerReview/CustomerReview.js
@@ -1,3 +1,5 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import useReviews from '../../Hooks/useReviews';
 
@@ -12,24 +14,46 @@ const CustomerReview = () => {
             <div className='grid lg:grid-cols-3 gap-20 my-16'>
 
                 <div className='p-5 h-96 rounded-lg border-2'>
-                    <img className='mx-auto rounded-full my-4' src={review1?.img} alt="" />
-                    <h2 className='text-xl font-medium my-7 text-center'>{review1?.name}</h2>
+                    <img className='mx-auto rounded-full' src={review1?.img} alt="" />
+                    <h2 className='text-xl font-medium my-2 text-center'>{review1?.name}</h2>
+                    <div className='text-center text-yellow-500 my-3'>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    </div>
                     <p title={review1?.review}>{review1?.review.length > 200 ? review1?.review.slice(0, 200) + "....." : review1?.review}</p>
                 </div>
 
                 <div className='p-5 h-96 rounded-lg border-2'>
-                    <img className='mx-auto rounded-full my-4' src={review2?.img} alt="" />
-                    <h2 className='text-xl font-medium my-7 text-center'>{review2?.name}</h2>
+                    <img className='mx-auto rounded-full' src={review2?.img} alt="" />
+                    <h2 className='text-xl font-medium my-2 text-center'>{review2?.name}</h2>
+                    <div className='text-center text-yellow-500 my-3'>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    </div>
                     <p title={review2?.review}>{review2?.review.length > 200 ? review2?.review.slice(0, 200) + "....." : review2?.review}</p>
                 </div>
 
                 <div className='p-5 h-96 rounded-lg border-2'>
-                    <img className='mx-auto rounded-full my-4' src={review3?.img} alt="" />
-                    <h2 className='text-xl font-medium my-7 text-center'>{review3?.name}</h2>
+                    <img className='mx-auto rounded-full' src={review3?.img} alt="" />
+                    <h2 className='text-xl font-medium my-2 text-center'>{review3?.name}</h2>
+                    <div className='text-center text-yellow-500 my-3'>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    </div>
                     <p title={review3?.review}>{review3?.review.length > 200 ? review3?.review.slice(0, 200) + "....." : review3?.review}</p>
                 </div>
             </div>
             <div className='flex justify-center'>
+
                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full my-6">
                     SHOW ALL
                 </button>
--- src/Components/EachReview/EachReview.js
+++ src/Components/EachReview/EachReview.js
@@ -1,3 +1,5 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const EachReview = ({ eachReview }) => {
@@ -8,8 +10,15 @@ const EachReview = ({ eachReview }) => {
     return (
         <div>
             <div className=' p-5 h-96 rounded-lg border-2'>
-                <img className='mx-auto rounded-full my-4' src={img} alt="" />
-                <h2 className='text-xl font-medium my-7 text-center'>{name}</h2>
+                <img className='mx-auto rounded-full' src={img} alt="" />
+                <h2 className='text-xl font-medium my-2 text-center'>{name}</h2>
+                <div className='text-center text-yellow-500 my-3'>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                </div>
                 <p title={review}>{review.length > 200 ? review.slice(0, 200) + "....." : review}</p>
             </div>
         </div>
