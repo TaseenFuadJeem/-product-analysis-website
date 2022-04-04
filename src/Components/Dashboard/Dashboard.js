f8bee1e874d86583c43f3ab8bd0b23d845b45a94
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-04 12:10:46 +0600

    One chart added in dhashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, []);
+
     return (
-        <div>
-            <h1>This is dashboard</h1>
+        <div className='mx-auto'>
+            <h1 className='text-center text-4xl font-semibold mt-20 my-10'>Our sales in 2021</h1>
+            <div className='flex justify-center'>
+                <LineChart width={800} height={500} data={data}>
+                    <Line type="monotone" dataKey="sell" stroke='#8884d8'></Line>
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" ></CartesianGrid>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+
         </div>
     );
 };
