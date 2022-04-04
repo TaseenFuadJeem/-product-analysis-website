f56f7c4b8a7c1354500660f5635e461791303e67
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-04-04 12:54:07 +0600

    First question done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>This is blogs</h1>
+            <div className='border-2 p-10 rounded-lg'>
+                <h1 className='text-4xl font-semibold mb-5'>What is Context API ?</h1>
+                <p>
+                    In a React App, there is a way to effectively produce global variables which is known as <strong>The React Context API.</strong> This is the opposite way to "pop drilling" or moving props from grandparent to child to parent and many more. Then comes the context API system in react. This is the easiest way to sent data between grandparent to child to parent. This context API is a feature which is added on react in version 16.3. It allows to share state across the entire app very lightly and very easily.
+                </p>
+            </div>
         </div>
     );
 };
